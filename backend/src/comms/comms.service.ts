08e038137d76a49ac923cba4804ede71c98d27ce
Jose <37120821+zerta92@users.noreply.github.com>
2025-02-09 10:09:41 +0000

    Update comms.service.ts

--- backend/src/comms/comms.service.ts
+++ backend/src/comms/comms.service.ts
@@ -8,7 +8,7 @@ import { parseMessage } from './utils';
 export class CommsService {
   async getNexDelivery(userId: string): Promise<Message | { error: string }> {
     /* 
-    I used a `find` instead of a `filte`r because from the data available there is only one instance per user id for the next delivery 
+    I used a `find` instead of a `filter` because from the data available there is only one instance per user id for the next delivery 
     but in reality there could be more and in that case I would need to find the next one by expected delivery date 
     */
     const scheduledDeliveries = (data as NextDelivery[])?.find(
